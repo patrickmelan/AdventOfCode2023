1b60f3b7e0f80472d9bf3c5b6f4da27b831d2ba7
Patrick Melan <patrick.melan@outlook.com>
2024-01-05 10:43:59 -0500

    Day 3

---
+++ AdventOfCode2023/Day03Gondola/Character.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day03Gondola
+{
+    internal class Character
+    {
+        public Character(char thing)
+        {
+            char charObj = thing;
+            string charAsAString;
+            bool symbol = !Char.IsLetter(charObj) && !Char.IsAsciiDigit(charObj);
+            if (!symbol)
+            {
+                charAsAString = charObj.ToString();
+            }
+        }
+    }
+}
